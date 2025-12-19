f60ed0702694add929aa6375a377561b84ad7aef
Laysa <laysabernardes.ads@gmail.com>
2025-12-18 22:18:27 -0300

    style: Ajuste forms projetos

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', 
-  //baseURL: 'https://site-v5hr.onrender.com',
+  //baseURL: 'http://localhost:3000', 
+  baseURL: 'https://site-v5hr.onrender.com',
 });
 
 api.interceptors.request.use(
